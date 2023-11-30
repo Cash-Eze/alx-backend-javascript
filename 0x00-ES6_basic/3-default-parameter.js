4ab77acb4d98165edb6fab8432255ad27230cecb
Cash-Eze <amoshiamatsu@gmail.com>
2023-11-30 03:07:55 -0800

    default param

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = undefined, expansion2019 = undefined) {
-  if (expansion1989) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019) {
-    expansion2019 = 19;
-  }
+export default function getSumOfHoods(initialNumber, expansion1989 = 897, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
+
