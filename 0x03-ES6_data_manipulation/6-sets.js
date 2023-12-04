16dff73c4772d2d2f70443a2687e6e3aef36aac4
Cash-Eze <amoshiamatsu@gmail.com>
2023-12-04 02:15:46 -0800

    data manipulation

---
+++ 0x03-ES6_data_manipulation/6-sets.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Amos Hiamatsu <https://github.com/cash-eze>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
